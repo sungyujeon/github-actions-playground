34519fcc9e223860f2d89d42de58b4951e5724bf
sungyujeon <sungyujeon91@gmail.com>
2023-05-19 20:52:12 +0900

    feat: build 시 test 통과

--- src/test/java/playground/healthCheck/HealthCheckApplicationTests.java
+++ src/test/java/playground/healthCheck/HealthCheckApplicationTests.java
@@ -2,10 +2,14 @@ package playground.healthCheck;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HealthCheckApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
